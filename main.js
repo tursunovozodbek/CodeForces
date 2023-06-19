86f3158064e3bb18d1059e2edb065fbf2d5d7298
Ozodbek <tursunovozodbek505@gmail.com>
2023-06-19 16:26:00 +0500

    Code Forces

---
+++ main.js
@@ -0,0 +1,31 @@
+// 02.04.2023
+// Tursunov Ozodbek - CodeForces
+// Dasturlashdagi bilimlarimni mustahkamlash uchun Codeforcesdagi masalalarni yechish
+
+// 1A - masala
+// let n = 12;
+// let m = 8;
+// let kafel = 5;
+// if ((n * m) % kafel == 0) {
+//   console.log("Kafel sindirmasdan teravergin!");
+// } else {
+//   console.log("Kafel sindirib tera qol!");
+// }
+
+// 1B - masala
+// let a = "R23C55";
+// let b = "BC23";
+// let temporary = b;
+// b = a;
+// a = temporary;
+// console.log(a, b);
+
+// 1C - masala
+// const inputData = [
+//   [0.0, 0.0],
+//   [1.0, 1.0],
+//   [0.0, 1.0],
+// ];
+
+// const copiedData = [...inputData];
+// console.log(copiedData);
